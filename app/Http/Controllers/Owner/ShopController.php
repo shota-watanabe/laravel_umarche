3f0b71d05cf7a036e1ce993e7db87a4fe1f3a58f
わたしょう <watasho@watashounoMacBook-Air.local>
2021-08-10 17:55:30 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -19,11 +20,11 @@ class ShopController extends Controller
             // dd(Auth::id()); //数字
 
             $id = $request->route()->parameter('shop'); //shopのid取得
-            if(!is_null($id)){ // null判定
+            if (!is_null($id)) { // null判定
             $shopsOwnerId = Shop::findOrFail($id)->owner->id;
                 $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
                 $ownerId = Auth::id();
-                if($shopId !== $ownerId){ // 同じでなかったら
+                if ($shopId !== $ownerId) { // 同じでなかったら
                     abort(404); // 404画面表示
                 }
             }
@@ -49,9 +50,15 @@ class ShopController extends Controller
     public function update(Request $request, $id)
     {
         $imageFile = $request->image; //一時保存
-        if(!is_null($imageFile) && $imageFile->isValid())
-        {
-            Storage::putFile('public/shops', $imageFile);
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+
+            // Storage::putFile('public/shops', $imageFile); リサイズなしの場合
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName. '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            // dd($imageFile, $resizedImage);
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
 
             return redirect()->route('owner.shops.index');
         }
