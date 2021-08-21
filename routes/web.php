f79179084f366524b6af35009613b1c69ae3ed22
わたしょう <watasho@watashounoMacBook-Air.local>
2021-08-21 18:54:31 +0900

    cart index

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('auth:users')->group(function () {
 });
 
 Route::prefix('carts')->middleware('auth:users')->group(function () {
+    Route::get('/', [CartController::class, 'index'])->name('cart.index');
     Route::post('add', [CartController::class, 'add'])->name('cart.add');
 });
 
