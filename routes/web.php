d683a790c8fb1780ab8f6ff9d520efe02fdc91cc
わたしょう <watasho@watashounoMacBook-Air.local>
2021-08-22 11:03:12 +0900

    stripe controller

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,11 @@ Route::middleware('auth:users')->group(function () {
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-Route::prefix('carts')->middleware('auth:users')->group(function () {
+Route::prefix('cart')->middleware('auth:users')->group(function () {
     Route::get('/', [CartController::class, 'index'])->name('cart.index');
     Route::post('add', [CartController::class, 'add'])->name('cart.add');
     Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+    Route::get('checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 });
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
